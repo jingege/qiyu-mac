9953b713124c04ef8f81c86b3d07a7080b695a46
张晋 <zhangjin@corp.netease.com>
2017-04-09 23:35:35 +0800

    Auto package libffmpeg to support mp3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,30 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    grunt.initConfig({
+        nwjs: {
+            options: {
+                version: '0.21.5',
+                buildDir: './build',
+//credits: './public/Credits.html',
+                macIcns: './app/qiyu.icns', // Path to the Mac icon file
+                platforms: ['osx64'],
+                flavor: 'normal' //https://github.com/nwjs/nw.js/wiki/Build-Flavors
+            },
+            src: './app/**/*' // Your NW.js app
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        src: 'libs/libffmpeg.dylib',
+                        dest: 'build/QiyuMac/osx64/QiyuMac.app/Contents/Versions/57.0.2987.133/nwjs Framework.framework/libffmpeg.dylib',
+                        flatten: true
+                    }
+                ]
+            }
+        }
+    });
 
-  grunt.initConfig({
-    nwjs: {
-      options: {
-        version: '0.21.5',
-        buildDir: './build',
-        //credits: './public/Credits.html',
-        macIcns: './app/qiyu.icns', // Path to the Mac icon file
-        platforms: ['osx64','win32'],
-        flavor: 'normal' //https://github.com/nwjs/nw.js/wiki/Build-Flavors
-      },
-      src: './app/**/*' // Your NW.js app
-    },
-  });
-
-  grunt.loadNpmTasks('grunt-nw-builder');
-  grunt.registerTask('default', ['nwjs']);
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-nw-builder');
+    grunt.registerTask('default', ['nwjs', 'copy']);
 };
